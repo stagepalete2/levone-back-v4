c22cf8ace5cc26d79417975d9b1139023df875a1
stagepalete <221744@astanait.edu.kz>
2026-02-17 20:03:56 +0500

    Dooglys fix

--- apps/tenant/stats/dooglys.py
+++ apps/tenant/stats/dooglys.py
@@ -206,7 +206,7 @@ class DooglysService:
             params['sale_point_id'] = branch_id
 
         # Убран двойной слеш /api/v1
-        result = self._make_request('GET', '/sales/order/list', params=params)
+        result = self._make_request('GET', '/api/v1/sales/order/list', params=params)
         
         if result is None:
             logger.warning(
