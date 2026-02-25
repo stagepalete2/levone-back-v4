f6223e01b784062e07843400e5e135b40db89041
stagepalete <221744@astanait.edu.kz>
2026-02-25 10:20:10 +0500

    Fix birthday

--- apps/tenant/stats/views.py
+++ apps/tenant/stats/views.py
@@ -341,7 +341,7 @@ class StatisticsDetailView(PeriodMixin, BranchMixin, BaseAdminStatsView, Templat
     def _get_birthday_greeting_clients(qs, date_from, date_to=None, branch_id=None):
         """Клиенты, получившие поздравление с ДР (MessageLog с birthday-типами)."""
         from apps.tenant.senler.models import MessageLog
-        log_filters = Q(campaign__message_type__in=['birthday_today', 'birthday_7days', 'birthday_1day'])
+        log_filters = Q(campaign__title__icontains="День Рождения")
         if date_from:
             log_filters &= Q(created_at__gte=date_from)
         if date_to:
